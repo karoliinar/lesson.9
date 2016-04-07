c894873b205e854ec345d08dd5c6c23f81987784
Karoliina Roots <karoliina.roots@gmail.com>
2016-04-07 10:42:32 +0300

    esimene fail

---
+++ login.php
@@ -0,0 +1,10 @@
+
+<h1>Sign up</h1>
+<form>
+
+	<input type="text" placeholder="username" name="username">
+	<input type="password" placeholder="password" name="password">
+	
+	<input type="submit" name="signup" value="Sign up">
+	
+</form>
